9caada582428adff67adcc2340e1af24357a79db
Baliang <670280772@qq.com>
2019-05-14 22:28:58 +0800

    20190514暂时不显示登录界面

--- src/store.js
+++ src/store.js
@@ -5,13 +5,7 @@ Vue.use(Vuex)
 
 //状态
 const state={
-  userInfo:JSON.parse(sessionStorage.getItem('userInfo')),
-  traces:[],
-  poetTraces:[],
-  tracesInfo:[],
-  poemsData:[],
-  poemsInfo:[],
-  mode:'轨迹'
+  userInfo:JSON.parse(sessionStorage.getItem('userInfo'))
 
 }
 
@@ -24,67 +18,7 @@ const mutations = {
     state.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
 
   },
-  setTraces (state,tracesData){
-    state.traces = []
-    state.tracesInfo = []
-    tracesData.forEach(function (value) {
-      let {startx,starty,endx,endy} = value
-      let {作家,朝代,年份,起始点,目的地省,目的地市县} = value
-      state.traces.push([[startx,starty],[endx,endy]])
-      let traceInfo = {
-        作家,
-        朝代,
-        年份,
-        起始点,
-        目的地省,
-        目的地市县
-      }
-      //console.log(traceInfo)
-      state.tracesInfo.push(traceInfo)
-    })
-    // console.log(state.traces)
-    //console.log(state.tracesInfo)
-  },
-  setPTraces (state,tracesData){
-    state.poetTraces = []
-    state.tracesInfo = []
-    tracesData.forEach(function (poet) {
-      let poetTrace = []
-      poet.forEach(function (value) {
-        let {startx,starty,endx,endy} = value
-        let {作家,朝代,年份,起始点,目的地省,目的地市县} = value
-        poetTrace.push([[startx,starty],[endx,endy]])
-        let traceInfo = {
-          作家,
-          朝代,
-          年份,
-          起始点,
-          目的地省,
-          目的地市县
-        }
-        //console.log(traceInfo)
-        state.tracesInfo.push(traceInfo)
-      })
-      state.poetTraces.push(poetTrace)
-    })
-    // console.log(state.poetTraces)
-    // console.log(state.tracesInfo)
-  },
-  setPoems(state,poemsData){
-    state.poemsData = []
-    state.poemsInfo = poemsData
-    poemsData.forEach(function (value) {
-      state.poemsData.push({
-        name: value.地点,
-        value: [value.经度,value.纬度,value.count]
-      })
-    })
-    //console.log(JSON.parse(JSON.stringify(state.poemsData)))
-  },
-  changeMode(state,newMode){
-    state.mode = newMode
-    console.log(state.mode)
-  }
+
 
 }
 
