3592286fd32b7cc30f87541ac9d557fdd621028c
Baliang <670280772@qq.com>
2019-05-16 15:54:04 +0800

    190516 添加组件

---
+++ src/components/CheckPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "check-page"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PoetList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "poet-list"
+    }
+</script>
+
+<style scoped>
+
+</style>
